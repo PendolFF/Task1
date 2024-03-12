b789da63460412ba5c62cef2460dc44d1b1cbe12
PendolFF <morev.ecrgo@gmail.com>
2024-03-12 15:28:32 +0700

    add data type c#

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("helo world");
+﻿//Console.WriteLine("helo world");
+
+// целочисленный
+int value = 100;
+Console.WriteLine(value);
+
+// целочисленный long позволяет хранить бОльшие типы данных чем int
+// дробные числа float и double (по аналогии с инит различия)
+double b = 5.55;
+// символьный тип char для храниения символов
+char c = '!';
+// логический булиев тип данных (true false)
+bool d = true;
+
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+
