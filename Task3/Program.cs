7d5f65c2426320e32ee03c88032bbb99d6119dbe
PendolFF <morev.ecrgo@gmail.com>
2024-03-12 16:53:34 +0700

    add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿int weight1 = 5;
+int weight2 = 8;
+int weight3 = 9;
+int weight4 = 2;
+
+int maxweight = weight1;
+
+//while (i < count);
+if (weight2 > maxweight)
+{
+   maxweight = weight2;
+}
+if (weight3 > maxweight)
+{
+   maxweight = weight3;
+}
+if (weight4 > maxweight)
+{
+   maxweight = weight4;
+}
+
+ Console.WriteLine(maxweight);
