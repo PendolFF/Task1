e7c2bb3450a0295328bdec99a97b672887bae828
PendolFF <morev.ecrgo@gmail.com>
2024-03-12 17:16:21 +0700

    add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+int sum = 1;
+
+while (sum <= number)
+{
+     Console.WriteLine(sum);
+    sum = sum + 1;
+    
+}
